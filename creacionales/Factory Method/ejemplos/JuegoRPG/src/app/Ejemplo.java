368b493e4bf80c0dd215de42e9b223e19f7373de
jitrivino <141260275+jitrivino@users.noreply.github.com>
2024-03-06 08:43:32 -0500

    Update Ejemplo.java

--- creacionales/Factory Method/ejemplos/JuegoRPG/src/app/Ejemplo.java
+++ creacionales/Factory Method/ejemplos/JuegoRPG/src/app/Ejemplo.java
@@ -9,7 +9,7 @@ public class Ejemplo {
         FabricaPersonaje fabricaGuerrero = new FabricaGuerrero("Conan", 100);
         Personaje guerrero = fabricaGuerrero.crearPersonaje();
 
-        System.out.println("Creando personaje Guerrero...");
+        System.out.println("Creando personaje Guerrero...1");
         guerrero.mostrarInformacion();
 
         System.out.println();
